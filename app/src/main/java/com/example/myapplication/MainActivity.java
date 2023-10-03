a3b751f1acc8bcfed05426e45c6faecbddd69e8a
tuitenrein <levu1409@gmail.com>
2023-10-03 12:07:18 +0700

    Add Login Activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_signUp;
+    Button btn_signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +19,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_signUp = findViewById(R.id.btn_signUp);
+        btn_signIn = findViewById(R.id.btn_signIn);
+
         btn_signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openRegisterActivity();
             }
         });
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogInActivity();
+            }
+        });
+    }
+
+    private void openLogInActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivities(new Intent[]{intent});
     }
 
     private void openRegisterActivity() {
