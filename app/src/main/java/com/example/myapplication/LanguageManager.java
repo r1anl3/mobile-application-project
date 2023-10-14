69fca2360d6f2d87550000560af148061be326fd
tuitenrein <levu1409@gmail.com>
2023-10-14 15:28:03 +0700

    Add globe icon.

---
+++ app/src/main/java/com/example/myapplication/LanguageManager.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
+import java.util.Locale;
+
+public class LanguageManager {
+    private Context ct;
+
+    public LanguageManager(Context ctx) {
+        this.ct = ctx;
+    }
+
+    public String checkResource() {
+        // Check current language
+        String langCode = Locale.getDefault().getLanguage();
+        return langCode;
+    }
+
+    public void updateResource(String code) {
+        // Update language
+        Locale locale = new Locale(code);
+        Locale.setDefault(locale);
+        Resources resources = ct.getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+    }
+}
