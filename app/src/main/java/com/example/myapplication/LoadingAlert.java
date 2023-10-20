c25bcafa618d5daf313c831e28c3b97ed1c740dd
tuitenrein <levu1409@gmail.com>
2023-10-20 15:49:39 +0700

    Use LoadingAlert for loading animation.

---
+++ app/src/main/java/com/example/myapplication/LoadingAlert.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+
+public class LoadingAlert {
+    Activity activity;
+    Activity dialog;
+    public LoadingAlert(Activity thisActivity) {
+        dialog = thisActivity;
+    }
+}
