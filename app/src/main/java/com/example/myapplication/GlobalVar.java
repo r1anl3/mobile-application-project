933c88139c21ba05e3c337881eefb46997191f03
tuitenrein <levu1409@gmail.com>
2023-11-07 20:35:55 +0700

    Update GlobalVar.java

--- app/src/main/java/com/example/myapplication/GlobalVar.java
+++ app/src/main/java/com/example/myapplication/GlobalVar.java
@@ -2,9 +2,11 @@ package com.example.myapplication;
 
 public class GlobalVar {
     public static String baseUrl = "https://uiot.ixxc.dev/";
-    public static String signUpUrl = baseUrl + "auth/realms/master/login-actions/";
+    public static String signUpUrl = baseUrl + "auth/realms/master/login-actions/registration";
+    public static String signInUrl = baseUrl + "auth/realms/master/login-actions/authenticate";
     public static String tokenUrl = baseUrl + "auth/realms/master/protocol/openid-connect/token";
-    public static String resetUrl = baseUrl + "auth/realms/master/account/password";
+    public static String resetPwsUrl = baseUrl + "auth/realms/master/login-actions/reset-credentials";
+    public static String changePwsUrl = baseUrl + "auth/realms/master/account/password";
     public static String LOG_TAG = "API LOG";
     public static int DELAY_TIME = 100;
 }
