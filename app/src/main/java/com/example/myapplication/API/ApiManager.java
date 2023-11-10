d37e7005cc967c5f7cef601ed5104f231882cd59
tuitenrein <levu1409@gmail.com>
2023-11-10 15:37:12 +0700

    Update getToken

--- app/src/main/java/com/example/myapplication/API/ApiManager.java
+++ app/src/main/java/com/example/myapplication/API/ApiManager.java
@@ -5,20 +5,29 @@ import android.util.Log;
 import com.example.myapplication.GlobalVar;
 import com.example.myapplication.Model.Token;
 
+import java.io.IOException;
+
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ApiManager {
-    private ApiClient client = new ApiClient();
-    private ApiService service = client.getClient().create(ApiService.class);
+    private static final ApiClient client = new ApiClient();
+    private static final ApiService service = client.getClient().create(ApiService.class);
 
-
-    public void getToken(String user, String pass) {
-        // Todo: Get token
+    public static void getToken(String user, String pass) {
         Call<Token> call = service.getToken(GlobalVar.grantType, GlobalVar.clientId, user, pass);
-//        try {
-//            Response<Token>
-//        }
+        try {
+            Response<Token> response = call.execute();
+            if (response.isSuccessful()) {
+                Token token = response.body();
+                assert token != null;
+                ApiClient.token = token.access_token;
+                Log.d(GlobalVar.LOG_TAG, "token: " + ApiClient.token);
+            }
+            else { Log.d(GlobalVar.LOG_TAG, "getToken: Not Successful"); }
+        } catch (IOException e) {
+//            e.printStackTrace();
+            Log.d(GlobalVar.LOG_TAG, "exception: " + e.getMessage());
+        }
     }
 }
