96d088b445d0406342a4d84a3053931c32684ed2
tuitenrein <levu1409@gmail.com>
2023-11-13 10:32:35 +0700

    Change getToken return value to Token

--- app/src/main/java/com/example/myapplication/API/ApiManager.java
+++ app/src/main/java/com/example/myapplication/API/ApiManager.java
@@ -3,6 +3,7 @@ package com.example.myapplication.API;
 import android.util.Log;
 
 import com.example.myapplication.GlobalVar;
+import com.example.myapplication.Manager.LocalDataManager;
 import com.example.myapplication.Model.Token;
 
 import java.io.IOException;
@@ -14,20 +15,21 @@ public class ApiManager {
     private static final ApiClient client = new ApiClient();
     private static final ApiService service = client.getClient().create(ApiService.class);
 
-    public static void getToken(String user, String pass) {
+    public static Token getToken(String user, String pass) {
         Call<Token> call = service.getToken(GlobalVar.grantType, GlobalVar.clientId, user, pass);
         try {
             Response<Token> response = call.execute();
             if (response.isSuccessful()) {
                 Token token = response.body();
-                assert token != null;
                 ApiClient.token = token.access_token;
                 Log.d(GlobalVar.LOG_TAG, "token: " + ApiClient.token);
+                return token;
             }
             else { Log.d(GlobalVar.LOG_TAG, "getToken: Not Successful"); }
         } catch (IOException e) {
 //            e.printStackTrace();
             Log.d(GlobalVar.LOG_TAG, "exception: " + e.getMessage());
         }
+        return null;
     }
 }
