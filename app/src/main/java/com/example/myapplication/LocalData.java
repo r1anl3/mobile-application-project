d8140e234f0868c2236a9a4e3fd2ff0858dc4d0f
tuitenrein <levu1409@gmail.com>
2023-11-13 10:32:08 +0700

    Update get set token to local

--- app/src/main/java/com/example/myapplication/LocalData.java
+++ app/src/main/java/com/example/myapplication/LocalData.java
@@ -1,11 +1,28 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 public class LocalData {
-    private Context context;
+    private static final String LOCAL_DATA = "LOCAL_DATA";
+    private Context ct;
 
-    public LocalData(Context context) {
-        this.context = context;
+    public LocalData(Context ct) {
+        // Initial LocalData
+        this.ct = ct;
+    }
+
+    public void putStringValue(String key, String value) {
+        // Put string value to SharedPreferences
+        SharedPreferences preferences = ct.getSharedPreferences(LOCAL_DATA, Context.MODE_PRIVATE); // Create SharedPreferences
+        SharedPreferences.Editor editor = preferences.edit(); // Create editor
+        editor.putString(key, value); // Put string to SharedPreferences
+        editor.apply(); // Save
+    }
+
+    public String getStringValue(String key) {
+        // Get string value from SharedPreferences
+        SharedPreferences preferences = ct.getSharedPreferences(LOCAL_DATA, Context.MODE_PRIVATE); // Create SharedPreferences
+        return preferences.getString(key, ""); // Return string, null if not found
     }
 }
