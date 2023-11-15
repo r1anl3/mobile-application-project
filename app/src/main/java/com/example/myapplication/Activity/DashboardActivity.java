d761d8e27fc8d1ce5056fb01b54aa74e6d9a5d57
tuitenrein <levu1409@gmail.com>
2023-11-15 14:28:58 +0700

    Add fragment and change fragment

--- app/src/main/java/com/example/myapplication/Activity/DashboardActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/DashboardActivity.java
@@ -3,17 +3,25 @@ package com.example.myapplication.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
+import com.example.myapplication.Fragment.DeviceFragment;
 import com.example.myapplication.Fragment.HomeFragment;
+import com.example.myapplication.Fragment.MapFragment;
+import com.example.myapplication.Fragment.UserFragment;
 import com.example.myapplication.R;
 
 public class DashboardActivity extends AppCompatActivity {
     private FragmentManager fm;
-    public HomeFragment homeFrag;
-    private Fragment fragment = null;
-    int selectedIndex;
+    private HomeFragment homeFrag;
+    private DeviceFragment devFrag;
+    private MapFragment mapFrag;
+    private UserFragment userFrag;
+    private ImageView ic_home, ic_dev, ic_map, ic_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +31,44 @@ public class DashboardActivity extends AppCompatActivity {
         InitialVars();
         InitialViews();
         InitialEvents();
-    }
 
-    private void InitialEvents() {
-        fm.beginTransaction().add(R.id.main_frame, homeFrag, "home").commit();
-        fragment = homeFrag;
-    }
-
-    private void InitialViews() {
+        fm.beginTransaction().add(R.id.main_frame, homeFrag, "home").commit(); // Add home fragment on create
+//        Fragment fragment = homeFrag;
     }
 
     private void InitialVars() {
-        fm = getSupportFragmentManager();
-        homeFrag = new HomeFragment(DashboardActivity.this);
+        // Initial variables
+        fm = getSupportFragmentManager(); // Create Fragment manager
+        homeFrag = new HomeFragment(DashboardActivity.this); // Create Home fragment
+        devFrag = new DeviceFragment(DashboardActivity.this);// Create Device fragment
+        mapFrag = new MapFragment(DashboardActivity.this); // Create Map fragment
+        userFrag = new UserFragment(DashboardActivity.this); // Create User fragment
     }
+
+    private void InitialViews() {
+        // Initial views
+        ic_home = findViewById(R.id.ic_home); // Find home icon
+        ic_dev = findViewById(R.id.ic_dev); // Find device icon
+        ic_map = findViewById(R.id.ic_map); // Find map icon
+        ic_user = findViewById(R.id.ic_user); // Find user icon
+    }
+
+    private void InitialEvents() {
+        // Initial events
+        ic_home.setOnClickListener(view -> replaceFragment(homeFrag, 0)); // Replace home fragment
+        ic_dev.setOnClickListener(view -> replaceFragment(devFrag, 1)); // Replace device fragment
+        ic_map.setOnClickListener(view -> replaceFragment(mapFrag, 2)); // Replace map fragment
+        ic_user.setOnClickListener(view -> replaceFragment(userFrag, 3)); // Replace user fragment
+    }
+
+    private void replaceFragment(Fragment fragment, int num) {
+        // Replace fragment
+        Log.d("something", "replaceFragment: " + num);
+        FragmentTransaction transaction = fm.beginTransaction(); // Create Fragment transaction
+        transaction.replace(R.id.main_frame, fragment); // Replace fragment
+        transaction.addToBackStack(null); // No call backstack
+        transaction.commit(); // Perform transaction
+    }
+
+
 }
