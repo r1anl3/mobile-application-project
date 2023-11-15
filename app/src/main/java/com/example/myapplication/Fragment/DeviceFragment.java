372b4ac906cb8bf0e3a73893510e7e5cb4208f9d
tuitenrein <levu1409@gmail.com>
2023-11-15 14:27:33 +0700

    Create DeviceFragment.java

---
+++ app/src/main/java/com/example/myapplication/Fragment/DeviceFragment.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.Activity.DashboardActivity;
+import com.example.myapplication.R;
+
+public class DeviceFragment extends Fragment {
+    DashboardActivity parentActivity;
+    public DeviceFragment() {
+        // Required empty public constructor
+    }
+
+    public DeviceFragment(DashboardActivity activity) {
+        this.parentActivity = activity;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_device, container, false);
+    }
+}
