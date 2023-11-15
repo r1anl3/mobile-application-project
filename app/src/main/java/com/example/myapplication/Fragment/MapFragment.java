5af689fd51ebd0918696a19a81f9f3e55d407b16
tuitenrein <levu1409@gmail.com>
2023-11-15 14:27:36 +0700

    Create MapFragment.java

---
+++ app/src/main/java/com/example/myapplication/Fragment/MapFragment.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.Activity.DashboardActivity;
+import com.example.myapplication.R;
+
+public class MapFragment extends Fragment {
+    DashboardActivity parentActivity;
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    public MapFragment(DashboardActivity activity) {
+        this.parentActivity = activity;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
