8960c0c57e0da82f3a1a3ed553bf3fdee815f54f
tuitenrein <levu1409@gmail.com>
2023-11-17 16:23:24 +0700

    Update getUser

--- app/src/main/java/com/example/myapplication/API/ApiManager.java
+++ app/src/main/java/com/example/myapplication/API/ApiManager.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.example.myapplication.GlobalVar;
 import com.example.myapplication.Manager.LocalDataManager;
 import com.example.myapplication.Model.Token;
+import com.example.myapplication.Model.User;
 
 import java.io.IOException;
 
@@ -16,20 +17,40 @@ public class ApiManager {
     private static final ApiService service = client.getClient().create(ApiService.class);
 
     public static Token getToken(String user, String pass) {
-        Call<Token> call = service.getToken(GlobalVar.grantType, GlobalVar.clientId, user, pass);
+        // Get token
+        Call<Token> call = service.getToken(GlobalVar.grantType,
+                GlobalVar.clientId,
+                user,
+                pass); // Call api get token using 4 fields
         try {
-            Response<Token> response = call.execute();
-            if (response.isSuccessful()) {
-                Token token = response.body();
-                ApiClient.token = token.access_token;
-                Log.d(GlobalVar.LOG_TAG, "token: " + ApiClient.token);
-                return token;
+            Response<Token> response = call.execute(); // Get response from server
+            if (response.isSuccessful()) { // If success
+                Token token = response.body(); // Assign response to token
+                ApiClient.token = token.access_token; // Update Api client token
+                Log.d(GlobalVar.LOG_TAG, "token: " + ApiClient.token); // Log Api client token
+                return token; // Return token
             }
-            else { Log.d(GlobalVar.LOG_TAG, "getToken: Not Successful"); }
+            else { Log.d(GlobalVar.LOG_TAG, "getToken: Not Successful"); } // If fail
         } catch (IOException e) {
 //            e.printStackTrace();
-            Log.d(GlobalVar.LOG_TAG, "exception: " + e.getMessage());
+            Log.d(GlobalVar.LOG_TAG, "exception: " + e.getMessage()); // Print exception
         }
-        return null;
+        return null; // Return null if exception
+    }
+
+    public static void getUser() {
+        // Get user info
+        Call<User> call = service.getUser(); // Call api get user
+        try {
+            Response<User> response = call.execute(); // Get response from server
+            if (response.isSuccessful()) { // If success
+                User.setMe(response.body()); // Set me with repose
+                Log.d(GlobalVar.LOG_TAG, "Username: " + User.getMe().getUsername()); // Log username
+            }
+            else { Log.d(GlobalVar.LOG_TAG, "getUser: Not Successful"); } // If fail
+        } catch (IOException e) {
+//            e.printStackTrace();
+            Log.d(GlobalVar.LOG_TAG, "exception: " + e.getMessage()); // Print exception
+        }
     }
 }
