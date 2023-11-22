fde566398314f528ed8d8b67230771532859dbe3
tuitenrein <levu1409@gmail.com>
2023-11-22 23:35:59 +0700

    Rename fragment

--- app/src/main/java/com/example/myapplication/Fragment/ChartFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/ChartFragment.java
@@ -29,6 +29,6 @@ public class ChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_device, container, false);
+        return inflater.inflate(R.layout.fragment_chart, container, false);
     }
 }
--- app/src/main/java/com/example/myapplication/Fragment/FeatureFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/FeatureFragment.java
@@ -58,7 +58,7 @@ public class FeatureFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_feature, container, false);
     }
 
     @Override
