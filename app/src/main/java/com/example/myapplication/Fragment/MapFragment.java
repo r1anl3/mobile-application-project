a38b700c6e712714bcfaa7235b32bcfecfb64aee
tuitenrein <levu1409@gmail.com>
2023-11-26 10:53:12 +0700

    Get geo location, show bottom sheet

--- app/src/main/java/com/example/myapplication/Fragment/MapFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/MapFragment.java
@@ -1,8 +1,11 @@
 package com.example.myapplication.Fragment;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,13 +17,19 @@ import androidx.fragment.app.Fragment;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.myapplication.API.ApiManager;
 import com.example.myapplication.Activity.DashboardActivity;
-import com.example.myapplication.Model.User;
+import com.example.myapplication.Model.Asset;
+import com.example.myapplication.Model.Attribute;
 import com.example.myapplication.R;
 
 import org.osmdroid.config.Configuration;
@@ -30,7 +39,6 @@ import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
-import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow;
 
 import java.util.ArrayList;
 
@@ -39,8 +47,9 @@ public class MapFragment extends Fragment {
     DashboardActivity parentActivity;
     Handler handler;
     MapView mapView;
-    double aLat = 10.869778736885038;
-    double aLong = 106.80280655508835;
+    double aLat;
+    double aLong;
+    Attribute attribute;
 
     public MapFragment() {
         // Required empty public constructor
@@ -69,6 +78,7 @@ public class MapFragment extends Fragment {
             boolean isOk = bundle.getBoolean("IS_OK"); // Get message data
             if (!isOk) return false; // If not ok
 
+            setMap(aLat, aLong);
             super.onViewCreated(view, savedInstanceState);
 
             return false;
@@ -80,13 +90,20 @@ public class MapFragment extends Fragment {
     }
 
     private void InitialEvents() {
-//        getInfo();
-        setMap(aLat, aLong);
+        getInfo();
     }
     private void getInfo() {
         // Get information about user, weather assets
         new Thread(() -> { // new thread
             ApiManager.getAsset(); // Get asset
+
+            attribute = Asset.getMe().getAttributes();
+            aLat = attribute.getLocation()
+                    .getGeoPoint()
+                    .getLat();
+            aLong = attribute.getLocation()
+                    .getGeoPoint()
+                    .getLong();
 
             Message msg = handler.obtainMessage(); // Create message
             Bundle bundle = new Bundle(); // Create bundle
@@ -97,37 +114,76 @@ public class MapFragment extends Fragment {
     }
 
     private void setMap(double aLat, double aLong) {
-        Context ctx = parentActivity.getApplicationContext();
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        Context ctx = parentActivity.getApplicationContext(); // Get parent activity context
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx)); // Get configuration
 
-        mapView.setTileSource(TileSourceFactory.MAPNIK);
-        mapView.getController().setZoom(18.0);
+        mapView.setTileSource(TileSourceFactory.MAPNIK); // Set title source
+        mapView.getController().setZoom(18.0); // Set zoom
 
         requestPermissionsIfNecessary(new String[]{
-                Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.INTERNET
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.INTERNET
+        }); // Required permissions
+        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS); // Set zoom controller
+        mapView.setMultiTouchControls(true); // Enable multitouch
+
+
+        CompassOverlay compassOverlay = new CompassOverlay(parentActivity.getApplicationContext(), mapView); // Create compass
+        compassOverlay.enableCompass(); // Enable compass
+        mapView.getOverlays().add(compassOverlay); // Add compass
+
+        GeoPoint point = new GeoPoint(aLat, aLong); // Create geo point
+
+        Marker startMarker = new Marker(mapView); // Create maker
+        startMarker.setPosition(point); // Set marker position
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER); // Set anchor center by width, height
+        startMarker.setOnMarkerClickListener((marker, mapView) -> {
+            showInfo(); // Show info bottom sheet on click marker
+            return true;
         });
-        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
-        mapView.setMultiTouchControls(true);
+        mapView.getOverlays().add(startMarker); // Show marker
+
+        mapView.getController().setCenter(point); // Center geo point
+    }
+
+    private void showInfo() {
+        // Show assets information
+        final Dialog dialog = new Dialog(parentActivity); // Create dialog
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // No title
+        dialog.setContentView(R.layout.bottomsheet); // Set bottom sheet
+
+        LinearLayout editLayout = dialog.findViewById(R.id.layoutEdit); // Get edit layout
+        Button btn_view = dialog.findViewById(R.id.btn_view); // Get view button
+        TextView tv_info = editLayout.findViewById(R.id.tv_info); // Get textview info
+
+        btn_view.setOnClickListener(view -> {
+            parentActivity.replaceFragment(new FeatureFragment()); // Replace feature fragment on click
+            dialog.dismiss(); // Hide bottom sheet
+        });
 
 
-        CompassOverlay compassOverlay = new CompassOverlay(parentActivity.getApplicationContext(), mapView);
-        compassOverlay.enableCompass();
-        mapView.getOverlays().add(compassOverlay);
+        String temp = "Temperature: " + attribute.getTemperature().getValue() + "\n";
+        String humid = "Humid: " + attribute.getHumidity().getValue() + "\n";
+        String geo = "Geo: " + aLat + " " + aLong + "\n";
+        String info = temp + humid + geo;
+        tv_info.setText(info); // Set info to bottom sheet
 
-        GeoPoint point = new GeoPoint(aLat, aLong);
+        dialog.show(); // Show bottom sheet
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT); // Match parent, wrap content
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // Background color
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; // Animation
+        dialog.getWindow().setGravity(Gravity.BOTTOM); // Layout gravity
 
-        Marker startMarker = new Marker(mapView);
-        startMarker.setPosition(point);
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
-        mapView.getOverlays().add(startMarker);
-
-        mapView.getController().setCenter(point);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (int i = 0; i < grantResults.length; i++) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+        // Request permissions
+        ArrayList<String> permissionsToRequest = new ArrayList<>(); // Permission array
+        for (int i = 0; i < grantResults.length; i++) { // Permission index
             permissionsToRequest.add(permissions[i]);
         }
         if (permissionsToRequest.size() > 0) {
@@ -153,5 +209,4 @@ public class MapFragment extends Fragment {
                     REQUEST_PERMISSIONS_REQUEST_CODE);
         }
     }
-
 }
