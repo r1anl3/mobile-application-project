53a2ce0016687f5f5a8175b377d4038dc7b5ca74
tuitenrein <levu1409@gmail.com>
2023-12-08 00:23:18 +0700

    Update chart

--- app/src/main/java/com/example/myapplication/Fragment/ChartFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/ChartFragment.java
@@ -5,29 +5,37 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.myapplication.Activity.DashboardActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.Service.ForegroundService;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
-import org.eazegraph.lib.charts.ValueLineChart;
-import org.eazegraph.lib.models.ValueLinePoint;
-import org.eazegraph.lib.models.ValueLineSeries;
-
 import java.util.ArrayList;
 
 public class ChartFragment extends Fragment {
     DashboardActivity parentActivity;
     LineChart lineChart;
+    ProgressBar pg_loading;
+    LineDataSet lineDataSet;
+    ArrayList<ILineDataSet> iLineDataSets;
+    LineData lineData;
+    ImageView btn_sync;
+    private final int[] lColor = {R.color.cerise, R.color.some_blue, R.color.wind, R.color.rain};
+    private final String[] lLabel = {"Temperature", "Humidity", "Wind speed", "Rain fall"};
 
     public ChartFragment() {
         // Required empty public constructor
@@ -48,30 +56,75 @@ public class ChartFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        InitialView(view);
+        InitialEvent();
         super.onViewCreated(view, savedInstanceState);
-        // Code here
-        lineChart = parentActivity.findViewById(R.id.lineChart);
-        LineDataSet lineDataSet = new LineDataSet(lineChartDataSet(),"data set");
-        ArrayList<ILineDataSet> iLineDataSets = new ArrayList<>();
+    }
+
+    private void InitialView(View view) {
+        lineChart = view.findViewById(R.id.lineChart);
+        pg_loading = view.findViewById(R.id.pg_loading);
+        btn_sync = view.findViewById(R.id.btn_sync);
+    }
+
+    private void InitialEvent() {
+        ArrayList<Float> sourceData = new ArrayList<>();
+
+        loadingAnimation();
+        btn_sync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadingAnimation();
+            }
+        });
+
+
+        int chosenData = 3;
+        switch (chosenData) {
+            case 0:
+                sourceData = ForegroundService.aTemp;
+                break;
+            case 1:
+                sourceData = ForegroundService.aHumid;
+                break;
+            case 2:
+                sourceData = ForegroundService.aSpeed;
+                break;
+            case 3:
+                sourceData = ForegroundService.aRain;
+                break;
+        }
+
+        int color = ContextCompat.getColor(parentActivity.getApplicationContext(),  lColor[chosenData]);
+        String label = lLabel[chosenData];
+
+        chartSetup(sourceData, label);
+        chartUI(color);
+    }
+
+    private void chartSetup(ArrayList<Float> sourceData, String label) {
+        lineDataSet = new LineDataSet(lineChartDataSet(sourceData), label);
+        iLineDataSets = new ArrayList<>();
         iLineDataSets.add(lineDataSet);
-
-        LineData lineData = new LineData(iLineDataSets);
+        lineData = new LineData(iLineDataSets);
         lineChart.setData(lineData);
-        lineChart.invalidate();
+    }
 
+    private void loadingAnimation() {
+        pg_loading.setVisibility(View.VISIBLE);
+        lineChart.setVisibility(View.INVISIBLE);
 
-        //if you want set background color use below method
-        //lineChart.setBackgroundColor(Color.RED);
+        new Handler().postDelayed(() -> {
+            pg_loading.setVisibility(View.INVISIBLE);
+            lineChart.setVisibility(View.VISIBLE);
+        }, 1000);
+    }
 
-        // set text if data are are not available
+    private void chartUI(int color) {
         lineChart.setNoDataText("Data not Available");
 
-        //you can modify your line chart graph according to your requirement there are lots of method available in this library
-
-        //now customize line chart
-
-        lineDataSet.setColor(Color.BLUE);
-        lineDataSet.setCircleColor(Color.GREEN);
+        lineDataSet.setColor(color);
+        lineDataSet.setCircleColor(color);
         lineDataSet.setDrawCircles(true);
         lineDataSet.setDrawCircleHole(true);
         lineDataSet.setLineWidth(5);
@@ -79,22 +132,15 @@ public class ChartFragment extends Fragment {
         lineDataSet.setCircleHoleRadius(10);
         lineDataSet.setValueTextSize(10);
         lineDataSet.setValueTextColor(Color.BLACK);
+        lineChart.invalidate();
     }
 
+    private ArrayList<Entry> lineChartDataSet(ArrayList<Float> sourceData){
+        ArrayList<Entry> dataSet = new ArrayList<>();
 
-    private ArrayList<Entry> lineChartDataSet(){
-        ArrayList<Entry> dataSet = new ArrayList<Entry>();
+        for (int i = 0; i < sourceData.size(); i++) {
+            dataSet.add(new Entry(i, sourceData.get(i)));
+        }
 
-        dataSet.add(new Entry(0,40));
-        dataSet.add(new Entry(1,10));
-        dataSet.add(new Entry(2,15));
-        dataSet.add(new Entry(3,12));
-        dataSet.add(new Entry(4,20));
-        dataSet.add(new Entry(5,50));
-        dataSet.add(new Entry(6,23));
-        dataSet.add(new Entry(7,34));
-        dataSet.add(new Entry(8,12));
-        return  dataSet;
-
-
+        return dataSet;
     }}
