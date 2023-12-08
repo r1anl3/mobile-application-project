a18ed131c76aebab57786669b8bf527333533e82
tuitenrein <levu1409@gmail.com>
2023-12-09 00:21:49 +0700

    Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,10 +40,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.code.gson:gson:2.10.1");
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.airbnb.android:lottie:6.1.0");
+    implementation("com.airbnb.android:lottie:6.1.0")
     implementation("com.squareup.okhttp3:logging-interceptor:3.4.1")
     implementation("org.osmdroid:osmdroid-android:6.1.6")
 
