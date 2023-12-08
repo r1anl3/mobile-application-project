5c6234fc83db24960ca3d5c89e551046bfb52772
tuitenrein <levu1409@gmail.com>
2023-12-09 00:20:49 +0700

    Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -53,8 +52,4 @@ dependencies {
     implementation("com.nineoldandroids:library:2.4.0")
 
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
 }
